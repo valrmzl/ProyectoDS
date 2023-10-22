c69f366b9612174e1f9e3583f82ceac478c69bd2
ChrisPereda0601 <christian.pereda@iteso.mx>
2023-10-22 11:40:39 -0600

    Manejo de animación de jugador

--- main.py
+++ main.py
@@ -3,36 +3,53 @@ from pygame.locals import *
 
 pygame.init()
 
-#ventana
+clock = pygame.time.Clock()
+fps = 60
+
+# ventana
 screen_width = 1000
 screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("El super juego de Vale, Chris y Sebas")
 
-#Variables del juego
+# Variables del juego
 tile_size = 50
 
 # cargar imagenes
-sun_img= pygame.image.load('img/sun.png')
-bg_img= pygame.image.load('img/sky.png')
+sun_img = pygame.image.load('img/sun.png')
+bg_img = pygame.image.load('img/sky.png')
 
-#Clase para mostrar mi jugador
+
+# Clase para mostrar mi jugador
 class Jugador():
     def __init__(self, x, y):
-        img = pygame.image.load('img/guy1.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+
+        for num in range(1, 5): #Cargaremos las 4 imagenes para la animación. Izquierda y derecha
+            img_right = pygame.image.load(f'img/guy{num}.png')
+            img_right = pygame.transform.scale(img_right, (40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.image = self.images_right[self.index]
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
     def update(self):
-        dx = 0  #Variables creadas para detectar colisiones
+        dx = 0  # Variables creadas para detectar colisiones
         dy = 0
+        walk_cooldown = 5
 
-        #Obtener clics
+        # Obtener clics
         key = pygame.key.get_pressed()
         if key[pygame.K_SPACE] and self.jumped == False:
             self.vel_y = -15
@@ -40,19 +57,42 @@ class Jugador():
         if key[pygame.K_SPACE] == False:
             self.jumped = False
         if key[pygame.K_LEFT]:
-            dx -= 5    #Le restamos 5 pixeles a la posición
+            dx -= 5  # Le restamos 5 pixeles a la posición
+            self.counter += 1
+            self.direction = -1
         if key[pygame.K_RIGHT]:
             dx += 5
+            self.counter += 1
+            self.direction = 1
+        #Si no se toca ninguna tecla regresaremos a la imagen original
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
 
-        #Agregar gravedad
+        #Manejo de animación
+        if self.counter > walk_cooldown:    #Neceario para la velocidad de caminata
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # Agregar gravedad
         self.vel_y += 1
         if self.vel_y > 10:
             self.vel_y = 10
         dy += self.vel_y
 
-        #Detectar colisiones
+        # Detectar colisiones
 
-        #Actualizar las coordenadas del jugador
+        # Actualizar las coordenadas del jugador
         self.rect.x += dx
         self.rect.y += dy
 
@@ -60,16 +100,17 @@ class Jugador():
             self.rect.bottom = screen_height
             dy = 0
 
-        #Dibujar jugador
+        # Dibujar jugador
         screen.blit(self.image, self.rect)
 
+
 class World():
-    #constructor
-    def __init__(self, data) -> None:
+    # constructor
+    def __init__(self, data):
 
         self.tile_list = []
-        #cargar imagenes
-        dirt_img= pygame.image.load('img/dirt.png')
+        # cargar imagenes
+        dirt_img = pygame.image.load('img/dirt.png')
         grass_img = pygame.image.load('img/grass.png')
 
         row_count = 0
@@ -77,64 +118,65 @@ class World():
             col_count = 0
             for tile in row:
                 if tile == 1:
-                    img = pygame.transform.scale(dirt_img,(tile_size,tile_size))
-                    #pero necesito saber donde la voy a poner
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    # pero necesito saber donde la voy a poner
                     img_rect = img.get_rect()
-                    img_rect.x = col_count* tile_size
-                    img_rect.y = row_count* tile_size
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 2:
-                    img = pygame.transform.scale(grass_img,(tile_size,tile_size))
-                    #pero necesito saber donde la voy a poner
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    # pero necesito saber donde la voy a poner
                     img_rect = img.get_rect()
-                    img_rect.x = col_count* tile_size
-                    img_rect.y = row_count* tile_size
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
-                col_count +=1 
-            row_count +=1
+                col_count += 1
+            row_count += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
 
 
-
-
-#esto es lo que me va a permitir ubicar las cosas que quiero mostrar
+# esto es lo que me va a permitir ubicar las cosas que quiero mostrar
 world_data = [
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
-[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
-[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
-[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+    [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
+    [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
+    [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-#Creación de instancias
+# Creación de instancias
 jugador = Jugador(100, screen_height - 130)
 world = World(world_data)
 
-#necesitamos un loop para que la ventana se muestre mucho tiempo
+# necesitamos un loop para que la ventana se muestre mucho tiempo
 # si no se cerraria
 run = True
 while run:
-    #le estoy diciendo en donde quiero que se muestre
+
+    clock.tick(fps)
+
+    # le estoy diciendo en donde quiero que se muestre
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
@@ -143,10 +185,10 @@ while run:
     jugador.update()
 
     for event in pygame.event.get():
-        #EL BOTON DE X
+        # EL BOTON DE X
         if event.type == pygame.QUIT:
             run = False
 
-        pygame.display.update() #sin esta linea no se veria el fondo
+    pygame.display.update()  # sin esta linea no se veria el fondo
 
 pygame.quit()
